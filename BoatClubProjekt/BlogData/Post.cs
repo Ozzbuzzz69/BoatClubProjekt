71f8b724d8effeab1498744c3434aef83b82089f
Nikolaj04 <felsdd04@gmail.com>
2024-12-02 09:56:27 +0100

    Post

---
+++ BoatClubProjekt/BlogData/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoatClubLibrary.BlogData
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string EventDescription { get; set; }
+
+        private static int NextId = 1;
+
+        public Post(string eventDescription)
+        {
+            Id = NextId++;
+            EventDescription = eventDescription;
+        }
+    }
+}
