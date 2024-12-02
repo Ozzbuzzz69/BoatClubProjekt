7e79bf0c7897c40c3109781b9b4b84250ddab81b
Nikolaj04 <felsdd04@gmail.com>
2024-12-02 10:08:31 +0100

    Blog

--- BoatClubProjekt/BlogData/Blog.cs
+++ BoatClubProjekt/BlogData/Blog.cs
@@ -8,5 +8,45 @@ namespace BoatClubLibrary.BlogData
 {
     public class Blog
     {
+        public Dictionary<int, Post> Posts {  get; set; } = new Dictionary<int, Post>();
+
+        public bool CreatePost(Post post)
+        {
+            if (post != null)
+            {
+                Posts.TryAdd(post.Id, post);
+                return true;
+            }
+            return false;
+        }
+
+        public Post? ReadPost(int id)
+        {
+            if (Posts.ContainsKey(id))
+            {
+                return Posts[id];
+            }
+            return null;
+        }
+
+        public bool UpdatePost(int id, string eventDescription)
+        {
+            if (Posts.ContainsKey(id))
+            {
+                Posts[id].EventDescription = eventDescription;
+                return true;
+            }
+            return false;
+        }
+
+        public bool DeletePost(int id)
+        {
+            if (Posts.ContainsKey(id))
+            {
+                Posts.Remove(id);
+                return true;
+            }
+            return false;
+        }
     }
 }
