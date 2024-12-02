b8f17ad35f12cbd5be296cf798942e47343aa77c
Ozzbuzzz69 <oscarfussings@outlook.dk>
2024-12-02 11:20:49 +0100

    id

--- BoatClubProjekt/AdminData/Admin.cs
+++ BoatClubProjekt/AdminData/Admin.cs
@@ -8,12 +8,13 @@ namespace BoatClubLibrary.AdminData
 {
     public class Admin : Person
     {
+        private static int NextId = 0;
         public int Id { get; set; }
 
-        public Admin(int id, string name, string telephoneNr, string address, string email) : base(name, address, email, telephoneNr)
+        public Admin(string name, string telephoneNr, string address, string email) : base(name, address, email, telephoneNr)
         {
-            id = Id;
-            
+            Id = ++NextId;
+
         }
     }
 }
