b0b6cb2534e710e789978c2ab33db7e65e27ca2d
Ozzbuzzz69 <oscarfussings@outlook.dk>
2024-12-03 12:36:55 +0100

    edsf

--- BoatConsole/Program.cs
+++ BoatConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BoatClubLibrary.BlogData;
 using BoatClubLibrary.BoatData;
+<<<<<<< HEAD
 using BoatClubLibrary.MemberData;
 using BoatClubLibrary;
 
@@ -17,6 +18,23 @@ memberLibrary.PrintAllMembers();
 Console.WriteLine(blog.ReadPost(1));
 
 //Console.WriteLine(blog.CreatePost("test2"));
+=======
+using BoatClubLibrary.MockData;
+
+//MemberService memberService = new MemberService();
+
+//memberService.GetMembers();
+
+Blog blog = new Blog();
+
+DateTime dt = new DateTime();
+
+Console.WriteLine(blog.CreatePost("test", dt));
+
+Console.WriteLine(blog.ReadPost(1));½
+
+Console.WriteLine(blog.CreatePost("test2", dt));
+>>>>>>> 17786dada4093a06a67d2a75d1e7398105de2a16
 
 Console.WriteLine(blog.ReadPost(2));
 
