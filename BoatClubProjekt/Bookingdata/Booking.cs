eb0c31daca41175ba1d72fc17631f34be296e9cc
Martin Frost Lauridsen <mal021@edu.zealand.dk>
2024-12-05 22:39:33 +0100

    Booking og repo

---
+++ BoatClubProjekt/Bookingdata/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoatClubLibrary.Bookingdata
+{
+    public class Booking
+    {
+        public static int NextId = 1;
+        public int Id { get; set; }
+
+    }
+}
---
+++ BoatClubProjekt/Bookingdata/BookingRepo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoatClubLibrary.Bookingdata
+{
+    public class BookingRepo
+    {
+        public static Dictionary<int, Booking> Bookings = new Dictionary<int, Booking>();
+
+        public bool AddBooking(Booking booking)
+        {
+            if (booking != null)
+            {
+                Bookings.TryAdd(booking.Id, booking);
+                return true;
+            }
+            return false;
+        }
+        public Booking? ReadBooking(int id)
+        {
+            if (Bookings.ContainsKey(id))
+            {
+                return Bookings[id];
+            }
+            return null;
+        }
+        public Booking Update(int bookingid,  Booking booking)
+        {
+            if (Bookings.ContainsKey(bookingid))
+            {
+                Bookings[bookingid] = booking;
+            }
+            return booking;
+        }
+        public bool DeleteBooking(int id)
+        {
+            if (Bookings.ContainsKey(id))
+            {
+                Bookings.Remove(id);
+                return true;
+            }
+            return false;
+        }
+    }
+}
