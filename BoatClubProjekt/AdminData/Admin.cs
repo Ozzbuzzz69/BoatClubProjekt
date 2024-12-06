ab4a1ad4f3c810e1e66045314dccf962b9878d4e
Nikolaj04 <felsdd04@gmail.com>
2024-12-06 12:25:14 +0100

    Admin bool remove

--- BoatClubProjekt/AdminData/Admin.cs
+++ BoatClubProjekt/AdminData/Admin.cs
@@ -8,19 +8,16 @@ namespace BoatClubLibrary.AdminData
 {
     public class Admin : Person
     {
-        private bool IsAdmin { get; set; }
-
         private static int NextId = 0;
 
-        public Admin(string name, string address, string email, string phoneNumber, bool isAdmin) : base(name, address, email, phoneNumber)
+        public Admin(string name, string address, string email, string phoneNumber) : base(name, address, email, phoneNumber)
         {
             Id = ++NextId;
-            IsAdmin = isAdmin;
         }
 
         public override string ToString()
         {
-            return $"Id: {Id}, Name: {Name}, Address {Address}, Email: {Email}, Phonenumber: {PhoneNumber}, Admin: {IsAdmin}";
+            return $"Id: {Id}, Name: {Name}, Address {Address}, Email: {Email}, Phonenumber: {PhoneNumber}";
         }
     }
 }
