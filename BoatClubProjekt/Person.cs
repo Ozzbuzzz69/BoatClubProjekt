678bc7450063cb216f1a90f3b5087df7a9aa2a07
MichaelTheDane <michael.schonemann@hotmail.dk>
2024-12-07 20:36:46 +0100

    Tiny constructor error fix

--- BoatClubProjekt/Person.cs
+++ BoatClubProjekt/Person.cs
@@ -11,13 +11,13 @@ namespace BoatClubLibrary
     {
         public int Id { get; set; }
         
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         
-        public string Address { get; set; }
+        public string Address { get; set; } = string.Empty;
         
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
         
-        public string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
 
         public Person() { }
 
