abb387464af07341f45521fde91bb1273672d729
MichaelTheDane <michael.schonemann@hotmail.dk>
2024-12-07 20:32:59 +0100

    Tiny constructor error fixes

--- BoatClubProjekt/MemberData/Member.cs
+++ BoatClubProjekt/MemberData/Member.cs
@@ -20,7 +20,7 @@ namespace BoatClubLibrary.MemberData
     public class Member : Person
     {   
         public MembershipType MembershipType { get; set; }
-        public string Birthday { get; set; }
+        public string Birthday { get; set; } = string.Empty;
 
         public bool IsRenting { get; set; }
 
@@ -30,7 +30,7 @@ namespace BoatClubLibrary.MemberData
         public Member(string name, string address, string email, string phoneNumber, MembershipType membershipType, 
             string birthday, bool isRenting) : base(name, address, email, phoneNumber)
         {
-            Id = ++NextId;
+            Id = NextId++;
             MembershipType = membershipType;
             Birthday = birthday;
             IsRenting = isRenting;
