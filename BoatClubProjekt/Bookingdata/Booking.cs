c16a30fb363c7b5251640bc210f49521942539f5
Nikolaj04 <felsdd04@gmail.com>
2024-12-10 12:04:08 +0100

    Booking

--- BoatClubProjekt/Bookingdata/Booking.cs
+++ BoatClubProjekt/Bookingdata/Booking.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BoatClubLibrary.BoatData;
+using BoatClubLibrary.MemberData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +12,23 @@ namespace BoatClubLibrary.Bookingdata
     {
         public int Id { get; set; }
 
+        public Member MemberId { get; set; }
+
+        public Boat BoatId { get; set; }
+
         private static int NextId = 1;
 
 
-        public Booking()
+        public Booking(Member memberId, Boat boatId)
         {
             Id = NextId++;
+            MemberId = memberId;
+            BoatId = boatId;
         }
 
         public override string ToString()
         {
-            return $"id: {Id}";
+            return $"Id: {Id}, MemberId: {MemberId}, BoatId {BoatId}";
         }
     }
 }
