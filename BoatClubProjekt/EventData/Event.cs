efaeefe288d488cfd898707c880c57c089592fcc
Nikolaj04 <felsdd04@gmail.com>
2024-12-11 10:09:46 +0100

    event og eventrepo

---
+++ BoatClubProjekt/EventData/Event.cs
@@ -0,0 +1,34 @@
+﻿using BoatClubLibrary.MemberData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoatClubLibrary.EventData
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string EventDate { get; set; }
+
+        public string EventDescription { get; set; }
+
+        public List<Member> JoinedMembers { get; set; } = new List<Member>();
+
+        private static int NextId = 1;
+
+        public Event(string eventDate, string eventDescription)
+        {
+            Id = NextId++;
+            EventDate = eventDate;
+            EventDescription = eventDescription;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Event date: {EventDate}, Event description: {EventDescription}";
+        }
+    }
+}
---
+++ BoatClubProjekt/EventData/EventRepo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoatClubLibrary.EventData
+{
+    public class EventRepo
+    {
+        public Dictionary<int, Event> Events { get; set; } = new Dictionary<int, Event>();
+
+        public bool CreateEvent(Event e)
+        {
+            if (e != null)
+            {
+                Events.TryAdd(e.Id, e);
+                return true;
+            }
+            return false;
+        }
+
+        public Event? ReadEvent(int id)
+        {
+            if (Events.ContainsKey(id))
+            {
+                return Events[id];
+            }
+            return null;
+        }
+
+        public bool UpdateEvent(int id, string eventDate, string eventDescription)
+        {
+            if (Events.ContainsKey(id))
+            {
+                Events[id].EventDate = eventDate;
+                Events[id].EventDescription = eventDescription;
+                return true;
+            }
+            return false;
+        }
+
+        public bool DeleteEvent(int id)
+        {
+            if (Events.ContainsKey(id))
+            {
+                Events.Remove(id);
+                return true;
+            }
+            return false;
+        }
+    }
+}
